8658abe1561396406e88362063874e30ecf0d521
Eugene <laser4622>
2019-11-09 14:58:00 +0300

    end

---
+++ src/components/End.jsx
@@ -0,0 +1,77 @@
+import React, {useEffect} from "react";
+import {Table, Divider, Tag} from 'antd';
+import {compare} from 'natural-orderby';
+
+const columns = [
+    {
+        title: 'Место',
+        dataIndex: 'place',
+        key: 'place'
+    },
+    {
+        title: 'Фамилия',
+        dataIndex: 'familyName',
+        key: 'familyName'
+    },
+    {
+        title: 'Имя',
+        dataIndex: 'name',
+        key: 'name'
+    },
+    {
+        title: 'Регион',
+        dataIndex: 'fullAddress',
+        key: 'fullAddress'
+    },
+    {
+        title: 'Очки',
+        dataIndex: 'points',
+        key: 'points'
+    },
+];
+
+const m = ["1 Филькина Ксения Мелеуз 42",
+    "2 Шайбакова Элина Уфа 42",
+    "3 Каримов Даниэль Сибай 41",
+    "4 Миннеханова Виктория с.Калмашбашево 41",
+    "5 Хурматова Ангелина Уфа 39",
+    "6 Цветцих Элла Ишимбай 39",
+    "7 gilyzetdinova liana Салават 39",
+    "8 Юлдашев Радмир Уфа 39",
+    "9 Деменина Александра Мелеуз 39",
+    "10 Тулупова Арина с.Бакалы 39",
+    "11 Шарифуллин Лука Стерлитамак 39",
+    "12 Альмакаева Лина Туймазы 39",
+    "13 Уразгулов Валерий с.Мишкино 39",
+    "14 Аделия Мухтасимова Нефтекамск 36",
+    "15 Мария Шовникова Салават 37",
+    "16 Елена Строева Салават 37",
+    "17 Иван Тарасов Уфа 37",
+    "18 Камилла Галиханова Дюртюли 37",
+    "19 Емельянова Кристина Салават 38",
+    "20 Низамова Аделина Иглинский_р-н 38",
+    "21 Лейсан Хамматова Уфа 36",
+    "22 Байгазина Айгуль Уфа 38",
+    "23 Букарева Анастасия Белебей 38",
+    "24 Диана Салихкулова Сибай 36",
+    "25 Салимгареева Карина с.Иглино 38",
+    "26 Ильяшенко Даниил Уфа 38",
+    "27 Шагибеков Артур с.Петровское 38",
+    "28 Чухнин Александр Салават 38",
+    "29 Мамбеткулова Эмилия Сибай 38",
+    "30 Петрова Мария Стерлитамак 38",
+    "31 Арсланов Азат с.Аскарово 37"];
+const End = () => {
+    const data = m.map(s => {
+        const [place, familyName, name, fullAddress, points] = s.split(' ');
+        return {place, familyName, name, fullAddress, points}
+    });
+
+    return <div style={{background: '#ffffff', minHeight: '100vh'}}>
+        <h1 style={{marginLeft: 10}}>Результаты</h1>
+        <Table columns={columns} dataSource={data.map((d, id) => ({key: id, ...d}))} pagination = {false}/>
+    </div>
+};
+
+export default End
+
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 import {GameContextProvider} from "./context";
 import App from "./App";
 import './index.css';
+import End from "./components/End";
 // import App from "./App";
 
 const Cover = () => (
@@ -15,7 +16,8 @@ const Cover = () => (
     </Router>
 );
 
-ReactDOM.render(<Cover/>, document.getElementById('root'));
+ReactDOM.render(<End/>, document.getElementById('root'));
+// ReactDOM.render(<Cover/>, document.getElementById('root'));
 
 // If you want your app to w    ork offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
